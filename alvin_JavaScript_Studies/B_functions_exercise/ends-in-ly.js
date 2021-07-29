98a54c0e73658572f972859216e91b5066c50eb4
mostwanted <klydetomasian@outlook.com>
2021-07-28 18:54:49 -0700

    update B functions exercise

---
+++ alvin_JavaScript_Studies/B_functions_exercise/ends-in-ly.js
@@ -0,0 +1,19 @@
+let endsInLy = function (str) {
+    if (str[str.length - 1] === 'y' && [str.length -2 === 'l']) {
+        return true;
+    }   else {
+        return false;
+    }
+}
+
+
+
+// Write a function `endsInLy` that accepts a string as an argument and returns a boolean indicating
+// whether or not the string ends in the substring 'ly'.
+
+console.log(endsInLy("pretty")); // false
+console.log(endsInLy("instant")); // false
+console.log(endsInLy("analytic")); // false
+console.log(endsInLy("timidly")); // true
+console.log(endsInLy("fly")); // true
+console.log(endsInLy("gallantly")); // true
