56ba6141cd58bfe50456b443d8555a66bf48d6c5
mostwanted <klydetomasian@outlook.com>
2021-08-06 15:42:34 -0700

    update loops exercise

---
+++ alvin_JavaScript_Studies/A_loops_exercise/snippet-1.js
@@ -0,0 +1,7 @@
+console.log("hello");  // hello
+
+for (let i = 0; i < 5; i++) {
+  console.log("code");  // code 5 times
+}
+
+console.log("goodbye"); // goodbye
---
+++ alvin_JavaScript_Studies/A_loops_exercise/snippet-2.js
@@ -0,0 +1,8 @@
+console.log("hi");  // hi
+
+for (let i = 3; i <= 7; i++) {
+  console.log("program");  // program 3 program 4 program 5 program 6 program 7
+  console.log(i);
+}
+
+console.log("bye");  // bye
