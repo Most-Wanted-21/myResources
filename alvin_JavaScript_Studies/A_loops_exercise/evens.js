9d93bc203f4dad412622275b2ded2fa246b8b625
mostwanted <klydetomasian@outlook.com>
2021-08-11 08:55:10 -0700

    update loops exercise

---
+++ alvin_JavaScript_Studies/A_loops_exercise/evens.js
@@ -0,0 +1,8 @@
+let evens = function (max) {
+  for (let i = 2; i < max; i += 2)
+  console.log(i)
+}
+
+evens(11);
+
+evens(8);
---
+++ alvin_JavaScript_Studies/A_loops_exercise/string-iterate.js
@@ -0,0 +1,6 @@
+let stringIterate = function (str) {
+  for (let i = str[0]; i > str.length; i++)
+  console.log(stringIterate[i]);
+}
+
+stringIterate("celery")
